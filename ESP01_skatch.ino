82fea2a5d4b9f60dde8d0b7fdf6221de22d7ae82
pp0025 <81042107+pp0025@users.noreply.github.com>
2024-07-13 01:46:23 +0530

    Add files via upload

---
+++ ESP01_skatch.ino
@@ -0,0 +1,53 @@
+/*************************************************************
+
+  You’ll need:
+   - Blynk IoT app (download from App Store or Google Play)
+   - ESP8266 board
+   - Decide how to connect to Blynk
+     (USB, Ethernet, Wi-Fi, Bluetooth, ...)
+
+  There is a bunch of great example sketches included to show you how to get
+  started. Think of them as LEGO bricks  and combine them as you wish.
+  For example, take the Ethernet Shield sketch and combine it with the
+  Servo example, or choose a USB sketch and add a code from SendData
+  example.
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL3l5lh8WLJ"
+#define BLYNK_TEMPLATE_NAME "light"
+#define BLYNK_AUTH_TOKEN "aVv_3XC4vWYC4NWxs1kdS0f161UYD7Jy"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "PPA0025";
+char pass[] = "akash@2024";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // You can also specify server:
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+ 
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+
+}
+
